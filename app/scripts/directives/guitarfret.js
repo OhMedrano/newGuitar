3bd296a8cc87e26e752e9507c16c4d630bad18e3
OhMedrano <Omedrano564@gmail.com>
2016-06-05 01:19:30 -0400

    Changes made to guitarFret.js and guitarString.js, Changing of root is now possible.

--- app/scripts/directives/guitarfret.js
+++ app/scripts/directives/guitarfret.js
@@ -13,22 +13,32 @@ angular.module('newGuitarApp')
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
        		
-      		scope.musicNotes = ["A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab"];
+
+
+       		//MUSIC NOTES
+       		//Sharps and Flats, for you music geeks
+      		scope.musicSharps = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
+      		scope.musicFlats = ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"];
+      		scope.musicNotes = scope.musicFlats;
+      		//MUSIC NOTES
+
+      		//VARIABLES FOR DISPLAY PURPOSES
+      		scope.changeRoots = 0;
+
+
+      		//VARIABLES FOR DISPLAY PURPOSES
+
+
 
       		//DEFAULT STRING SETTINGS
-           	scope.string1 = 7;
+           /*	scope.string1 = 7;
         	scope.string2 = 2;
         	scope.string3 = 10;
         	scope.string4 = 5;
         	scope.string5 = 0;
-        	scope.string6 = 7;
+        	scope.string6 = 7;*/
 
-        	scope.strings = [scope.string1,
-        					 scope.string2,
-        					 scope.string3,
-        					 scope.string4,
-        					 scope.string5,
-        					 scope.string6];
+        	scope.strings = [7,2,10,5,0,7];
       		//DEFAULT STRING SETTINGS
 
       		//SCALE FORUMLAS
@@ -64,17 +74,36 @@ angular.module('newGuitarApp')
 
       		//CHORD FORUMLAS
 
+      		//ALTERNATE TUNING FORMULAS
+      		scope.tunings = [
+      				{"name":"Standard","forumla":[7,2,10,5,0,7]},
+      				{"name":"D Tuning","forumla":[5,0,8,3,11,5]},
+      				{"name":"C Tuning","forumla":[3,10,6,1,8,3]},
+      				{"name":"B Tuning","forumla":[2,11,5,0,7,2]},
+      				{"name":"Full Step Down","forumla":[5,0,8,3,10,5]},
+      				{"name":"Minor Third","forumla":[6,3,0,9,6,3]},
+      				{"name":"All Fourths","forumla":[7,3,8,5,0,8]},
+      				{"name":"Major Sixth","forumla":[0,3,6,9,0,3]},
+      				{"name":"Open A","forumla":[7,0,7,4,0,7]},
+      				{"name":"Open B","forumla":[6,2,9,2,9,2]},
+      				{"name":"Open C","forumla":[7,3,10,3,10,3]},
+      				{"name":"Open D","forumla":[5,0,9,5,0,5]},
+      				{"name":"DADDAD","forumla":[5,0,5,5,0,5]},
+      				{"name":"Cello/Standard","forumla":[7,2,0,5,11,7]},
+      				{"name":"Hot Type","forumla":[5,0,9,7,2,0]},
+      				{"name":"Augmented Fourths","forumla":[9,3,9,3,9,3]}
+
+
+      		];
+      		//ALTERNATE TUNING FORMULAS
+
+
       		//!!!FUNCTION!!!
       		//Change the root of the string
       		scope.changeNote = function(string,note){
-      			var note = this;
-      			var strin = 'scope.string'+string;
+      			console.log(scope.strings[string]);
 
-      			console.log(strin);
-      			console.log(note.$index);
-
-      			string = note.$index;
-
+      			scope.strings[string] = this.$index;
       			
       			
       		};
@@ -83,7 +112,13 @@ angular.module('newGuitarApp')
 
       		//!!!FUNCTION!!!
 
+      		scope.changeGroup = function(chang){
+      			console.log(chang);
+      			scope.strings = chang;
 
+      			return scope.strings;
+
+      		};
 
 
 
--- app/scripts/directives/guitarstring.js
+++ app/scripts/directives/guitarstring.js
@@ -10,15 +10,67 @@ angular.module('newGuitarApp')
   .directive('guitarString', function () {
     return {
     	scope:{
+    		notes:'=notes',
     		root: '=root'
     	},
       templateUrl:'templates/guitarString.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.musicNotes = ["A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab"];
-      	
+        	//INTIALIZE VARIABLES
+        	//Old way until I figure out a good way to do this recursively
+        	scope.note0 = (scope.root + 0) %12,
+        	scope.note1 = (scope.root + 1) %12,
+        	scope.note2 = (scope.root + 2) %12,
+        	scope.note3 = (scope.root + 3) %12,
+        	scope.note4 = (scope.root + 4) %12,
+        	scope.note5 = (scope.root + 5) %12,
+        	scope.note6 = (scope.root + 6) %12,
+        	scope.note7 = (scope.root + 7) %12,
+        	scope.note8 = (scope.root + 8) %12,
+        	scope.note9 = (scope.root + 9) %12,
+        	scope.note10 = (scope.root + 10) %12,
+        	scope.note11 = (scope.root + 11) %12;
+        	//
+        	//BUILD ARRAY OF NOTES
+        	scope.newNotes = [];
+        	//
+        	console.log(scope.notes)
 
-        console.log(scope.musicNotes[scope.root]);
+
+        	 scope.$watchGroup(['root','notes','newNotes'],function(newVal,oldVal){
+      			//WATCH FOR NEW VALUES
+      			scope.root = newVal[0];
+      			scope.notes = newVal[1];
+      			scope.newNotes = angular.fromJson(newVal[2]);
+      			//WATCH FOR NEW VALUES
+
+
+      			//NEW NOTES
+      			scope.note0 = (scope.root + 0) %12,
+        		scope.note1 = (scope.root + 1) %12,
+        		scope.note2 = (scope.root + 2) %12,
+        		scope.note3 = (scope.root + 3) %12,
+        		scope.note4 = (scope.root + 4) %12,
+        		scope.note5 = (scope.root + 5) %12,
+        		scope.note6 = (scope.root + 6) %12,
+        		scope.note7 = (scope.root + 7) %12,
+        		scope.note8 = (scope.root + 8) %12,
+        		scope.note9 = (scope.root + 9) %12,
+        		scope.note10 = (scope.root + 10) %12,
+        		scope.note11 = (scope.root + 11) %12;
+        		//NEW NOTES
+
+      			
+
+
+
+
+
+        	 }) ;
+
+
+
+        
 
 
 
