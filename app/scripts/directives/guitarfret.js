5d7b66ec74239a08e268550c861046b72ccd4aa7
OhMedrano <Omedrano564@gmail.com>
2016-06-19 00:02:27 -0400

    Finished UI highlight

--- app/scripts/directives/guitarfret.js
+++ app/scripts/directives/guitarfret.js
@@ -15,10 +15,15 @@ angular.module('newGuitarApp')
        		
 
 
+          scope.selectedScaleNote = null;
+          scope.selectedChordNote = null;
+          scope.selectedChord = null;
+          scope.selectedScale = null;  
+
        		//MUSIC NOTES
        		//Used to store the notes.
-      		scope.musicSharps = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
-      		scope.musicFlats = ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"];
+      		scope.musicSharps = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'];
+      		scope.musicFlats = ['A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab'];
       		scope.musicNotes = scope.musicFlats;
       		scope.music = 3;
       		scope.musicRoot = 3; //Starts off at C
@@ -39,17 +44,17 @@ angular.module('newGuitarApp')
                   scope.mobileFret = function(){
                         scope.mobileFretString = !scope.mobileFretString;
 
-                  }
+                  };
                   scope.mobileScale = function(){
                         scope.mobileFretScale = !scope.mobileFretScale;
                         scope.noteSwitched = true;
                         scope.mobileFretChord = false;
-                  }
+                  };
                    scope.mobileChord = function(){
                         scope.mobileFretChord = !scope.mobileFretChord;
                         scope.noteSwitched = false;
                         scope.mobileFretScale = false;
-                  }
+                  };
 
       		//VARIABLES FOR DISPLAY PURPOSES
 
@@ -68,44 +73,44 @@ angular.module('newGuitarApp')
 
       		//SCALE FORUMLAS AND SCALE VARIABLES
       		scope.scaleIntervals = [
-      								{"name":"Tonic","secName":"Octave","colors":"F2291A"},
-      								{"name":"Minor Second","secName":"Minor Ninth","colors":"E58324"},
-      								{"name":"Major Second","secName":"Major Ninth","colors":"FCB11B"},
-      								{"name":"Minor Third","secName":"Minor Tenth","colors":"F2CE33"},
-      								{"name":"Major Third","secName":"Major Tenth","colors":"FCED2F"},
-      								{"name":"Perfect Fourth","secName":"Perfect 11th","colors":"BCE536"},
-      								{"name":"Augmented Fourth","secName":"Augmented 11th","colors":"64FC2F"},
-      								{"name":"Perfect Fifth","secName":"Perfect 12th","colors":"33E5BC"},
-      								{"name":"Minor Sixth","secName":"Minor 13th","colors":"2b61f2"},
-      								{"name":"Major Sixth","secName":"Major 13th","colors":"8F34E5"},
-      								{"name":"Minor Seventh","secName":"Minor 14th","colors":"E92DFC"},
-      								{"name":"Major Seventh","secName":"Major 14th","colors":"F24493"}  								
+      								{'name':'Tonic','secName':'Octave','colors':'F2291A'},
+      								{'name':'Minor Second','secName':'Minor Ninth','colors':'E58324'},
+      								{'name':'Major Second','secName':'Major Ninth','colors':'FCB11B'},
+      								{'name':'Minor Third','secName':'Minor Tenth','colors':'F2CE33'},
+      								{'name':'Major Third','secName':'Major Tenth','colors':'FCED2F'},
+      								{'name':'Perfect Fourth','secName':'Perfect 11th','colors':'BCE536'},
+      								{'name':'Augmented Fourth','secName':'Augmented 11th','colors':'64FC2F'},
+      								{'name':'Perfect Fifth','secName':'Perfect 12th','colors':'33E5BC'},
+      								{'name':'Minor Sixth','secName':'Minor 13th','colors':'2b61f2'},
+      								{'name':'Major Sixth','secName':'Major 13th','colors':'8F34E5'},
+      								{'name':'Minor Seventh','secName':'Minor 14th','colors':'E92DFC'},
+      								{'name':'Major Seventh','secName':'Major 14th','colors':'F24493'}  								
       		]
       		scope.allScale = [		  
-                          			        {"name":"Major/Ionian","scale":[0,2,4,5,7,9,11]},
-      						        {"name":"Minor/Aeolian","scale":[0,2,3,5,7,8,10]},
-      						        {"name":"Dorian","scale":[0,2,3,5,7,9,10]},
-      						        {"name":"Phrygian","scale":[0,1,3,5,7,8,10]},
-      						        {"name":"Lydian","scale":[0,2,4,6,7,9,11]},
-      						        {"name":"Mixolydian","scale":[0,2,4,5,7,9,10]},
-      						        {"name":"Locrian","scale":[0,1,3,5,6,8,10]},
-      						        {"name":"Minor Pentatonic","scale":[0,3,5,7,10]},
-      						        {"name":"Major Pentatonic","scale":[0,2,4,7,9]},
-      						        {"name":"Minor Blues","scale":[0,3,5,6,7,10]},
-      						        {"name":"Harmonic Minor","scale":[0,2,3,5,7,8,11]},
-      						        {"name":"Melodic Minor","scale":[0,2,3,5,7,9,11]},
-      						        {"name":"Diminished / 8-Tone","scale":[0,2,3,5,6,8,11]},
-                                                  {"name":"Persian","scale":[0,1,4,5,6,8,10]},
-                                                  {"name":"Hirajoshi","scale":[0,2,3,7,8]},
-                                                  {"name":"Kumoi","scale":[0,2,3,7,9]},
-                                                  {"name":"Chinese","scale":[0,4,6,7,11]},
-                                                  {"name":"Egyptian","scale":[0,2,5,7,10]},
-                                                  {"name":"Hungarian Gypsy","scale":[0,1,4,7,9]},
-                                                  {"name":"Romanian","scale":[0,2,3,6,7,9,10]},
-                                                  {"name":"Neopolitan Minor","scale":[0,1,3,5,7,8,11]},
-                                                  {"name":"Neopolitan Major","scale":[0,1,4,6,7,8,10]},
-                                                  {"name":"Prometheus","scale":[0,2,4,6,10]},
-                                                  {"name":"Ritusen","scale":[0,2,5,7,9]}
+                          			        {'name':'Major/Ionian','scale':[0,2,4,5,7,9,11]},
+      						        {'name':'Minor/Aeolian','scale':[0,2,3,5,7,8,10]},
+      						        {'name':'Dorian','scale':[0,2,3,5,7,9,10]},
+      						        {'name':'Phrygian','scale':[0,1,3,5,7,8,10]},
+      						        {'name':'Lydian','scale':[0,2,4,6,7,9,11]},
+      						        {'name':'Mixolydian','scale':[0,2,4,5,7,9,10]},
+      						        {'name':'Locrian','scale':[0,1,3,5,6,8,10]},
+      						        {'name':'Minor Pentatonic','scale':[0,3,5,7,10]},
+      						        {'name':'Major Pentatonic','scale':[0,2,4,7,9]},
+      						        {'name':'Minor Blues','scale':[0,3,5,6,7,10]},
+      						        {'name':'Harmonic Minor','scale':[0,2,3,5,7,8,11]},
+      						        {'name':'Melodic Minor','scale':[0,2,3,5,7,9,11]},
+      						        {'name':'Diminished / 8-Tone','scale':[0,2,3,5,6,8,11]},
+                                                  {'name':'Persian','scale':[0,1,4,5,6,8,10]},
+                                                  {'name':'Hirajoshi','scale':[0,2,3,7,8]},
+                                                  {'name':'Kumoi','scale':[0,2,3,7,9]},
+                                                  {'name':'Chinese','scale':[0,4,6,7,11]},
+                                                  {'name':'Egyptian','scale':[0,2,5,7,10]},
+                                                  {'name':'Hungarian Gypsy','scale':[0,1,4,7,9]},
+                                                  {'name':'Romanian','scale':[0,2,3,6,7,9,10]},
+                                                  {'name':'Neopolitan Minor','scale':[0,1,3,5,7,8,11]},
+                                                  {'name':'Neopolitan Major','scale':[0,1,4,6,7,8,10]},
+                                                  {'name':'Prometheus','scale':[0,2,4,6,10]},
+                                                  {'name':'Ritusen','scale':[0,2,5,7,9]}
       						];                                                                      
       		//current Scale set for Major Scale
       		scope.currentScale = scope.allScale[0].scale;
@@ -120,14 +125,14 @@ angular.module('newGuitarApp')
                   scope.currentChord = [];
                   scope.pickedChord = [];
       		 scope.musicChords = [
-                        {"name":"Major","forumla":[0,4,7],"color":"#F2847E"},
-                        {"name":"Minor","forumla":[0,3,7],"color":"#7CB1E5"},
-                        {"name":"6th ","forumla":[0,4,7,9],"color":"#BB67FC"},
-                        {"name":"7th ","forumla":[0,4,7,10],"color":"#E7CAFC"},
-                        {"name":"9th ","forumla":[0,4,7,10,2],"color":"#EDF2A6"},
-                        {"name":"13th","forumla":[0,4,7,10,2,5,9],"color":"#CBD9E8"},
-                        {"name":"Major 7th","forumla":[0,4,7,11],"color":"#9DE58B"},
-                        {"name":"Minor 7th","forumla":[0,3,7,10],"color":"#F2CBB6"}
+                        {'name':'Major','forumla':[0,4,7],'color':'#F2847E'},
+                        {'name':'Minor','forumla':[0,3,7],'color':'#7CB1E5'},
+                        {'name':'6th ','forumla':[0,4,7,9],'color':'#BB67FC'},
+                        {'name':'7th ','forumla':[0,4,7,10],'color':'#E7CAFC'},
+                        {'name':'9th ','forumla':[0,4,7,10,2],'color':'#EDF2A6'},
+                        {'name':'13th','forumla':[0,4,7,10,2,5,9],'color':'#CBD9E8'},
+                        {'name':'Major 7th','forumla':[0,4,7,11],'color':'#9DE58B'},
+                        {'name':'Minor 7th','forumla':[0,3,7,10],'color':'#F2CBB6'}
           		];
           	
           	scope.chordName;
@@ -138,22 +143,22 @@ angular.module('newGuitarApp')
 
       		//ALTERNATE TUNING FORMULAS
       		scope.tunings = [
-      				{"name":"Standard","forumla":[7,2,10,5,0,7]},
-      				{"name":"D Tuning","forumla":[5,0,8,3,11,5]},
-      				{"name":"C Tuning","forumla":[3,10,6,1,8,3]},
-      				{"name":"B Tuning","forumla":[2,11,5,0,7,2]},
-      				{"name":"Full Step Down","forumla":[5,0,8,3,10,5]},
-      				{"name":"Minor Third","forumla":[6,3,0,9,6,3]},
-      				{"name":"All Fourths","forumla":[7,3,8,5,0,8]},
-      				{"name":"Major Sixth","forumla":[0,3,6,9,0,3]},
-      				{"name":"Open A","forumla":[7,0,7,4,0,7]},
-      				{"name":"Open B","forumla":[6,2,9,2,9,2]},
-      				{"name":"Open C","forumla":[7,3,10,3,10,3]},
-      				{"name":"Open D","forumla":[5,0,9,5,0,5]},
-      				{"name":"DADDAD","forumla":[5,0,5,5,0,5]},
-      				{"name":"Cello/Standard","forumla":[7,2,0,5,11,7]},
-      				{"name":"Hot Type","forumla":[5,0,9,7,2,0]},
-      				{"name":"Augmented Fourths","forumla":[9,3,9,3,9,3]}
+      				{'name':'Standard','forumla':[7,2,10,5,0,7]},
+      				{'name':'D Tuning','forumla':[5,0,8,3,11,5]},
+      				{'name':'C Tuning','forumla':[3,10,6,1,8,3]},
+      				{'name':'B Tuning','forumla':[2,11,5,0,7,2]},
+      				{'name':'Full Step Down','forumla':[5,0,8,3,10,5]},
+      				{'name':'Minor Third','forumla':[6,3,0,9,6,3]},
+      				{'name':'All Fourths','forumla':[7,3,8,5,0,8]},
+      				{'name':'Major Sixth','forumla':[0,3,6,9,0,3]},
+      				{'name':'Open A','forumla':[7,0,7,4,0,7]},
+      				{'name':'Open B','forumla':[6,2,9,2,9,2]},
+      				{'name':'Open C','forumla':[7,3,10,3,10,3]},
+      				{'name':'Open D','forumla':[5,0,9,5,0,5]},
+      				{'name':'DADDAD','forumla':[5,0,5,5,0,5]},
+      				{'name':'Cello/Standard','forumla':[7,2,0,5,11,7]},
+      				{'name':'Hot Type','forumla':[5,0,9,7,2,0]},
+      				{'name':'Augmented Fourths','forumla':[9,3,9,3,9,3]}
 
 
       		];
@@ -193,6 +198,7 @@ angular.module('newGuitarApp')
       		};
       		scope.pickToneChord = function(note){
       			scope.music = note;
+
       			
       		};
 
@@ -209,16 +215,32 @@ angular.module('newGuitarApp')
       		
       		//!!!FUNCTION!!!
       		//Picking a chord 
-      		scope.pickChord = function(chord){
+      		scope.pickChord = function(chord,$index){
       			scope.pickedChord = chord.forumla;
       			scope.chordName = chord.name;
       			scope.chordColors = chord.color;
-      			
+           
 
       		};
 
 
+
       		//!!!FUNCTION!!!
+          //!!!FUNCTION!!!
+          //Selection highlights
+          scope.highLightScaleNote = function(index){
+            scope.selectedScaleNote = index;
+          };
+          scope.highLightScale = function(index){
+            scope.selectedScale = index;
+          };
+          scope.highLightingChordNote = function(index){
+            scope.selectedChordNote = index;
+          };
+          scope.highLightingChord = function(index){
+            scope.selectedChord = index;
+            console.log(scope.selectedChord);
+          };
 
       		//!!!FUNCTION!!!
       		//Choosing the notes for the given chord 
